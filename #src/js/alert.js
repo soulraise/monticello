e588e5e700fc9f4d2644cfa8cf4be660cc611d6f
Anton Rudenko <rudenko.a84@gmail.com>
2021-06-23 11:24:02 +0300

    notlast

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -43,3 +43,45 @@ function findMap() {
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 }
 findMap();
+
+
+let windowHeight = window.innerHeight;
+window.addEventListener('resize', function () {
+	windowHeight = window.innerHeight;
+})
+let headerHeight = document.getElementById('header').clientHeight;
+
+for (let link of document.getElementsByClassName('menu__item')) {
+	let sectionId = link.getAttribute('href');
+	window.addEventListener('scroll', function (e) {
+		let pos = document.querySelector(sectionId).getBoundingClientRect();
+		if (pos.top < windowHeight / 2 && pos.top > - 1 * (pos.height - windowHeight / 2)) {
+			link.firstElementChild.classList.add('menu__circle_active')
+		} else {
+			link.firstElementChild.classList.remove('menu__circle_active')
+		}
+	})
+}
+for (let link of document.getElementsByClassName('menu__item')) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+		let idSection = this.getAttribute('href');
+		let pos = document.querySelector(idSection)
+		window.scrollTo({
+			top: pos.offsetTop - headerHeight,
+			left: 0,
+			behavior: 'smooth',
+		})
+	})
+}
+function circleDown() {
+	document.getElementById('circle-down').addEventListener('click', function (e) {
+		e.preventDefault();
+		window.scrollTo({
+			top: document.getElementById('project').offsetTop - headerHeight,
+			left: 0,
+			behavior: 'smooth',
+		})
+	})
+}
+circleDown()
